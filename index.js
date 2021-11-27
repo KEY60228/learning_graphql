93ac1927e94879b727032830eb197a28d8387ad5
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-11-27 17:44:40 +0900

    5-2-1. ルートリゾルバ

--- index.js
+++ index.js
@@ -4,11 +4,26 @@ const typeDefs = `
     type Query {
         totalPhotos: Int!
     }
+
+    type Mutation {
+        postPhoto(name: String! description: String): Boolean!
+    }
 `
+
+// 写真を格納するための配列を定義する
+var photos = []
 
 const resolvers = {
     Query: {
-        totalPhotos: () => 42
+        // 写真を格納した配列の長さを返す
+        totalPhotos: () => photos.length
+    },
+    // postPhotoミューテーションと対応するリゾルバ
+    Mutation: {
+        postPhoto(parent, args) {
+            photos.push(args)
+            return true
+        }
     }
 }
 
