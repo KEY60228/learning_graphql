93e359556a0a3088543946a12f5517882c8ae3dd
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-12-01 23:07:26 +0900

    6-3-2-1. Apollo Cientでqueryを実行

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ApolloClient, { gql } from 'apollo-boost'
+
+const client = new ApolloClient({ uri: 'http://localhost:4000/graphql' })
+
+const query = gql`
+    {
+        totalUsers
+        totalPhotos
+    }
+`
+
+client.query({query})
+    .then(({ data }) => console.log('data', data))
+    .catch(console.error)
 
 ReactDOM.render(
   <React.StrictMode>
