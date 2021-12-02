60e4ef0659470fc8c3ea7b77dc3d0590aea7a8e6
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-12-02 22:33:40 +0900

    6-3-3-3. refetchとpolling

--- client/src/Users.js
+++ client/src/Users.js
@@ -5,17 +5,18 @@ import { ROOT_QUERY } from './App'
 const Users = () => {
     return (
         <Query query={ROOT_QUERY}>
-            {({ data, loading }) => 
-                loading ? <p>loading users...</p> : <UserList count={data.totalUsers} users={data.allUsers} />
+            {({ data, loading, refetch }) => 
+                loading ? <p>loading users...</p> : <UserList count={data.totalUsers} users={data.allUsers} refetchUsers={refetch} />
             }
         </Query>
     )
 }
 
-const UserList = ({ count, users }) => {
+const UserList = ({ count, users, refetchUsers }) => {
     return (
         <div>
             <p>{count} Users</p>
+            <button onClick={() => refetchUsers()}>Refetch Users</button>
             <ul>
                 {users.map(user =>
                     <UserListItem
