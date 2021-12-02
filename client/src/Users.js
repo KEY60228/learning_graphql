a47fe89773b52b12f6693b7e729174372882e257
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-12-02 22:29:56 +0900

    6-3-3-2. Usersの表示

--- client/src/Users.js
+++ client/src/Users.js
@@ -5,12 +5,41 @@ import { ROOT_QUERY } from './App'
 const Users = () => {
     return (
         <Query query={ROOT_QUERY}>
-            {result => {
-                return (
-                    <p>Users are loading: {result.loading ? "yes" : "no"}</p>
-                )
-            }}
+            {({ data, loading }) => 
+                loading ? <p>loading users...</p> : <UserList count={data.totalUsers} users={data.allUsers} />
+            }
         </Query>
+    )
+}
+
+const UserList = ({ count, users }) => {
+    return (
+        <div>
+            <p>{count} Users</p>
+            <ul>
+                {users.map(user =>
+                    <UserListItem
+                        key={user.githubLogin}
+                        name={user.name}
+                        avatar={user.avatar}
+                    />
+                )}
+            </ul>
+        </div>
+    )
+}
+
+const UserListItem = ({ name, avatar }) => {
+    return (
+        <li>
+            <img
+                src={avatar}
+                width={48}
+                height={48}
+                alt=""
+            />
+            {name}
+        </li>
     )
 }
 
