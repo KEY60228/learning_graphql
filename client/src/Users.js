5bdf2828a71ea1da08ea51f2c234319435325200
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-12-02 22:29:30 +0900

    6-3-3-1. お試しQueryコンポーネント

---
+++ client/src/Users.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Query } from 'react-apollo'
+import { ROOT_QUERY } from './App'
+
+const Users = () => {
+    return (
+        <Query query={ROOT_QUERY}>
+            {result => {
+                return (
+                    <p>Users are loading: {result.loading ? "yes" : "no"}</p>
+                )
+            }}
+        </Query>
+    )
+}
+
+export default Users
